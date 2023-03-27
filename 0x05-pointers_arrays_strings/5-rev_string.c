00134e23401bcb57949ec9f6b6551daa969d77e1
Ib-uth <iamlonelypoet@gmail.com>
2023-03-27 14:07:00 +0100

    Pointers, and arrays

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: input string
+ * Return: no return
+ */
+
+void rev_string(char *s)
+{
+	int count = 0, b, c;
+	char d;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	c = count - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
+}
