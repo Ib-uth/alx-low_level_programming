736e9fd9b85b6d3e676e7fcb60a2b8779f7e730e
Ibraheem Uthman <iamlonelypoet@gmail.com>
2023-04-28 02:50:31 +0100

    final

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,9 +6,6 @@
  */
 void myStartupFun(void) __attribute__ ((constructor));
 
-/**
- * myStartupFun - implementation of myStartupFun
- */
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,15 @@
 /**
  * free_list - Realease the memory allocated for a list
  *
- * @head: A pointer to the first node of the list to free
+ * @first: A pointer to the first node of the list to free
  */
-void free_list(list_t *head)
+void free_list(list_t *first)
 {
-	if (head)
+	if (first)
 	{
-		free_list(head->next);
-		if (head->str)
-			free(head->str);
-		free(head);
+		free_list(first->next);
+		if (first->str)
+			free(first->str);
+		free(first);
 	}
 }
