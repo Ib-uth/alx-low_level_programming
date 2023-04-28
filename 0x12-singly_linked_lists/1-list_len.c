c825e461b89fdc7a139d8cf729e571dedb981653
Ibraheem Uthman <iamlonelypoet@gmail.com>
2023-04-28 01:46:01 +0100

    updated 01

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,15 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include <stddef.h>
 #include "lists.h"
 
-/**
- * list_len - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
- */
 size_t list_len(const list_t *h)
 {
-	int count = 0;
+    size_t count = 0;
 
-	while (h)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
+    while (h != NULL)
+    {
+        count++;
+        h = h->next;
+    }
+
+    return (count);
 }
