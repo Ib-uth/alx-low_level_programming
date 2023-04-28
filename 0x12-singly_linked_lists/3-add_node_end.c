dc9958131d5de7aea82e7511fc5befaff99ac559
Ibraheem Uthman <iamlonelypoet@gmail.com>
2023-04-28 02:45:19 +0100

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,47 +8,33 @@
  *
  * Return: length of string
  */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
 
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * add_node_end - adds a new node to the end of linked list
- * @head: double pointer to a linked list
- * @str: string to add to the new node
- *
- * Return: pointer to the new node
- */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *tmp;
+	list_t *new, *temp;
+	size_t str_length = 0;
 
 	if (str == NULL)
-		return (NULL);
+		str_length = 0;
+	while (str[str_length] != '\0')
+		str_length++;
+
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(new->str);
+	new->len = str_length;
 	new->next = NULL;
-	if (*head == NULL)
-	{
+	temp = *head;
+	if (temp == NULL)
 		*head = new;
-		return (new);
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
 	}
-	tmp = *head;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = new;
-	return (new);
+
+	return (*head);
 }
