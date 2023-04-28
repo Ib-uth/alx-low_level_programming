fc30016ae253e6ed92590b370a8d650358d6b2c2
Ibraheem Uthman <iamlonelypoet@gmail.com>
2023-04-28 02:30:35 +0100

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,30 +7,30 @@
  *
  * Return: number of elements in the list
  */
-size_t list_len(const list_t *h)
-{
-    size_t count = 1;
-
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-
-    return (count);
-}
-
-
 // size_t list_len(const list_t *h)
 // {
-// 	size_t node_count = 1;
-// 	if (h == NULL)
-// 		return (0);
-// 	while (h->next != NULL)
-// 	{
-// 		h = h->next;
-// 		node_count++;
-// 	}
+//     size_t count = 1;
 
-// 	return (node_count);
+//     while (h != NULL)
+//     {
+//         count++;
+//         h = h->next;
+//     }
+
+//     return (count);
 // }
+
+
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 1;
+	if (h == NULL)
+		return (0);
+	while (h->next != NULL)
+	{
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
